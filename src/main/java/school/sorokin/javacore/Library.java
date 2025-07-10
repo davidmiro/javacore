05b96372dcedaddbeed4cb51bca081399cbbe79d
david <davidmiro15@gmail.com>
2025-07-10 11:44:11 -0400

    Проект по модулю Basics Exception

---
+++ src/main/java/school/sorokin/javacore/Library.java
@@ -0,0 +1,49 @@
+package school.sorokin.javacore;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> catalog;
+
+    public Library() {
+        catalog = new ArrayList<>();
+    }
+    public void addBook(int availableCopies, String title, String author) {
+        catalog.add(new Book(availableCopies, title, author));
+    }
+
+    public void takeBook(String title) throws BookNotFoundException, NoAvailableCopiesException {
+        Book book = findBookByTitle(title);
+        if (book == null) {
+            throw new BookNotFoundException("Book not found: " + title);
+        }
+        if (book.getAvailableCopies() == 0) {
+            throw new NoAvailableCopiesException("No copies available for: " + title);
+        }
+        book.setAvailableCopies(book.getAvailableCopies() - 1);
+    }
+
+    public void returnBook(String title) throws BookNotFoundException{
+        Book book = findBookByTitle(title);
+        if (book == null) {
+            throw new BookNotFoundException("Book not found: " + title);
+        }
+        book.setAvailableCopies(book.getAvailableCopies() + 1);
+    }
+
+    public List<Book> getAllBooks() {
+        return catalog;
+    }
+
+    private Book findBookByTitle(String title) {
+        for (Book book : catalog) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
+
---
+++ src/main/java/school/sorokin/javacore/NoAvailableCopiesException.java
@@ -0,0 +1,7 @@
+package school.sorokin.javacore;
+
+public class NoAvailableCopiesException extends Exception {
+    public NoAvailableCopiesException(String message) {
+        super(message);
+    }
+}
